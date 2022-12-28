bf186a2e3ab4ec2a5fa55d0edf3e4cb9b41e37a4
Irianto Mauduta <i-iriantomauduta@dana.id>
2022-12-28 15:18:21 +0700

    komit ketiga

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,8 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello Kambing!");
 
-
-
-
         System.out.println("Komit kedua");
+
+        System.out.println("Komit ketiga");
     }
 }
